474546961a8824b6c0ca6d4211fb332343abc6a3
mybil <mybil@NICOLE>
2023-03-05 21:41:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BookStoreRecord
+{
+    class Program
+    {
+        public record bookstore (int ID, string Author, string Title, double Price);
+
+        public static void Main()
+        {
+
+            bookstore bookstore = new(1,"Jane", "Doe", 20);
+            Console.WriteLine(bookstore);
+
+            bookstore bookstore2 = new(2, "Bill", "Nye", 25);
+            Console.WriteLine(bookstore2);
+
+            bookstore bookstore3 = new(3, "John", "Smith", 30);
+            Console.WriteLine(bookstore3);
+        }
+    }
+}
